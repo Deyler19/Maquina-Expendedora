119524fdd6e2b2d5e7602ff152ba5ef8e6fe83e4
arturomills <arturomills@gmail.com>
2022-08-19 14:33:24 -0400

    Csv 1.0

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ import re
 lett_patter = '^[A-Za-z]+$'
 num_patter = '^[0-9]+$'
 
+lista_seleccion = []
+
+def escribir(li):
+    li = []
+    with open('DB_CSV.csv', newline='',mode= 'w') as db:
+        escritura = csv.writer(li)
 
 
 print("productos disponibles")
@@ -14,6 +20,7 @@ producto= pd.read_csv('DB_CSV.csv')
 producto= producto.to_string(index=False)
 print (producto)
 
+
 while True:
     introducir_dinero=input("introduzca dinero ")
     if re.fullmatch(num_patter, (introducir_dinero)):
@@ -21,23 +28,32 @@ while True:
 while True:
     introducir_datos=input("que desea comprar?")
     if re.fullmatch(num_patter, introducir_datos):
+        lista_seleccion.append(introducir_datos)
         break
 
 with open('DB_CSV.csv', newline='',mode= 'r+') as db:
         lector = csv.reader(db , delimiter=',')
         for row in lector:
             if introducir_datos in row[0]:
-                print(f' Producto seleccionado cuesta {row[2]} y usted cuenta con {introducir_dinero}')
-                if int(introducir_dinero) >= int(row[2]):
-                    devuelta=int(introducir_dinero)-int(row[2])
-                    print (f'su producto {row [1]} esta disponible, su cambio es {devuelta} ')
-                    break
-                elif int(introducir_dinero) < int(row[2]):
+                stock = int(row[3])
+
+                if stock <= 0:
+                    print("No hay Articulo Disponible, Seleccione Otro")
+                
+                else: 
+
+                    print(f' Producto seleccionado cuesta {row[2]} y usted cuenta con {introducir_dinero}')
+                    if int(introducir_dinero) >= int(row[2]):
+                            devuelta=int(introducir_dinero)-int(row[2])
+                            
+                            print (f'su producto {row [1]} esta disponible, su cambio es {devuelta} ')
+                            break
+                    elif int(introducir_dinero) < int(row[2]):
                         print ("echa ma' cualto maldito pobre")
                         break
         else:
             print ("Producto no encontrado, elige bien maldito animal")
-
+print(lista_seleccion,)
 
 
 
